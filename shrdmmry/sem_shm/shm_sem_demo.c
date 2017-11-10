21db2f406e7d462d490d01dc5f8c6fb0bce7f372
somesh deshmukh <someshdeshmukh07@gmail.com>
2017-11-10 19:25:06 +0530

    ping pong memory

---
+++ shrdmmry/sem_shm/shm_sem_demo.c
@@ -0,0 +1,49 @@
+#include<sys/resource.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/mman.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<semaphore.h>
+#define MAX 128
+
+
+void main()
+{
+	int *count;
+	int fd;
+	sem_t *sem1,*sem2;
+	sem1=sem_open("/sem_1",O_CREAT,0600,1);
+	sem2=sem_open("/sem_2",O_CREAT,0600,0);
+	fd=shm_open("shrdcount",O_CREAT|O_RDWR,0600);
+	ftruncate(fd,MAX);
+	count=mmap(NULL,MAX,PROT_WRITE|PROT_READ,MAP_SHARED,fd,0);
+        if(fork()==0)
+	{
+		printf("%d:in parent before lock\n",getpid());
+		sem_wait(sem1);
+		printf("%d:in parent before lock:before +10 count\n",getpid());
+		printf("parent count=%d\n",*count);
+		*count+=10;
+		printf("parent count=%d\n",*count);
+		printf("%d:in parent after lock:before +10 count\n",getpid());
+                sem_post(sem2);
+		printf("%d:in parent aftre lock\n",getpid());
+	}
+	else
+	{
+		 printf("%d:in child before lock\n",getpid());
+		 sem_wait(sem2);
+		 printf("%d:in child before lock:before +10 count\n",getpid());
+		printf("child count=%d\n",*count);
+		*count+=100;
+		printf("child count=%d\n",*count);
+ 	  	 printf("%d:in child after lock:before +10 count\n",getpid());
+                sem_post(sem1);
+                printf("%d:in child aftre lock\n",getpid());
+                shm_unlink("shrdcount");
+	}
+}
+
---
+++ shrdmmry/sh_mem/reciever.c
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<sys/mman.h>
+#include<pthread.h>
+#include<fcntl.h>
+#include<sys/stat.h>
+#include<unistd.h>
+
+int *count;
+int main()
+{
+  int fd;
+  fd=shm_open("sharedcount",O_CREAT |O_RDWR,S_IRUSR|S_IWUSR);
+  ftruncate(fd,sizeof(int *));
+  count=mmap(NULL,sizeof(int *),PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+  printf("%d\n",*count);
+  sleep(2);
+  shm_unlink("sharedcount");
+  return 0;
+}              
